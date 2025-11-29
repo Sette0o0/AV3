68edc3c81c18575c9ad6eb27e9e094b8072fb1fe
Sette0o0 <rafaelsette12345@gmail.com>
2025-11-29 09:41:50 -0300

    fix: func_id errado removido de etapa

---
+++ backend/prisma/migrations/20251129122612_segundo_teste/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `func_id` on the `etapa` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `etapa` DROP COLUMN `func_id`;
